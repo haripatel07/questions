9b7d459adbe5943e76fac26ec1ab25bf92772aaa
yashvi-26 <147155058+yashvi-26@users.noreply.github.com>
2023-12-03 18:09:23 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,5 +1,6 @@
 x=input("Enter the string :")
 n=x[::-1]
+flag=0
 if(x==n):
     print("String is palindrome")
     quit
@@ -7,18 +8,14 @@ else:
    for i in range(len(x)-1,0,-1):
        if(x[0]==x[i]):
            y=i
-           break
-       else :
-           y=-1
-if(y!=-1):
-    n=x[:y+1:]
-    m=x[y::-1]   
-    if (n==m):
-            palin=x[-1:y:-1]+x     
-    else:
-            palin=x[-1:0:-1]+x
-else:
-            palin=x[-1:0:-1]+x
+           n=x[:y+1:]
+           m=x[y::-1]   
+           if (n==m):
+               palin=x[-1:y:-1]+x 
+               flag=1
+               break
+if(flag==0):
+    palin=x[-1:0:-1]+x
 print("the new palindrone string is ",palin)        
 
        
